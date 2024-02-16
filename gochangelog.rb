6f7edb9f6d47ba37955e8079fa12832cce51acf1
Richard Egeli <richard.stromland.egeli@intoto.io>
2024-02-16 02:37:45 +0100

    chore: brew file

--- gochangelog.rb
+++ gochangelog.rb
@@ -2,7 +2,7 @@ class Gochangelog < Formula
   desc "Automatically generate changelogs from your git commit history"
   homepage "https://github.com/richard-egeli/gochangelog"
   url "https://github.com/richard-egeli/gochangelog/archive/v1.0.0.tar.gz"
-  sha256 "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+  sha256 "531608f54cf7983c321f12f1b82e659a33c802f2392da4388469810a724cd117"
 
   depends_on "go" => :build
 
